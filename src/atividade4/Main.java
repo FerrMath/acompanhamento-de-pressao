abec675a5dbef967e5a2e9d45fd487b36863acd9
Matheus A <maf_dev@hotmail.com>
2023-09-04 20:03:01 -0300

    Initial commit
    Created the base view for the program with accessibility details

---
+++ src/atividade4/Main.java
@@ -0,0 +1,18 @@
+package atividade4;
+
+import atividade4.views.MainView;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ma_fe
+ */
+public class Main {
+    public static void main(String[] args) {
+        new MainView().setVisible(true);
+    }
+}
